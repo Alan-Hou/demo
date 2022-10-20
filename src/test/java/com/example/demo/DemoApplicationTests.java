767dd3997f284ac6c2228837632c433f9177b6de
kunple <kunple@qq.com>
2022-10-20 22:21:55 +0800

    update sofaboot verison

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,14 +3,11 @@ package com.example.demo;
 import com.example.demo.core.DecisionCoreEngine;
 import com.example.demo.enums.BizTypeEnums;
 import com.example.demo.model.DecisionContext;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
 public class DemoApplicationTests {
 
     @Autowired
